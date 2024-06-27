0fae002b290d5a3a5c321528c52f6fd0e20a607e
Sarah Colin <srh.colin@gmail.com>
2024-06-27 21:21:23 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -4,21 +4,27 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <p>
+        Salut tu seras un logo <code>src/App.js</code> et toi une nav.
+      </p>
+      <a
+        className="App-link"
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        toi rien mais tu es un lien donc ça peut me servir d'exemple
+      </a>
+    </header>
+  </div>
+
+
+
+
+
+
   );
 }
 
